6f634201f09cc8906b47ee3011874c5ef6ab8648
Yasin Cifci <cifci@campus.tu-berlin.de>
2022-10-27 15:27:16 +0200

    simple echo server

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65432 
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+if __name__ == '__main__':
+    main()
